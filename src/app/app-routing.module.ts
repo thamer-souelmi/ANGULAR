85c04b381755abe90f44455225bbda13d725ed39
Montaha Metjaouel <montaha.metjaouel@esprit.tn>
2024-04-06 23:43:12 +0100

    template FrontOffice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AllTemplateFrontComponent} from "./FrontOffice/all-template-front/all-template-front.component";
+import {HomeFrontComponent} from "./FrontOffice/home-front/home-front.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: AllTemplateFrontComponent,
+    children:[
+      {path:"home", component:HomeFrontComponent }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllTemplateFrontComponent } from './FrontOffice/all-template-front/all-template-front.component';
+import { FooterFrontComponent } from './FrontOffice/footer-front/footer-front.component';
+import { HeaderFrontComponent } from './FrontOffice/header-front/header-front.component';
+import { HomeFrontComponent } from './FrontOffice/home-front/home-front.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllTemplateFrontComponent,
+    FooterFrontComponent,
+    HeaderFrontComponent,
+    HomeFrontComponent
   ],
   imports: [
     BrowserModule,
