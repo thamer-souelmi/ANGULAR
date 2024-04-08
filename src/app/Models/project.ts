e7232ab8ae804fb4dca699a994cd697839b392f6
malekfr <malek.frikhi@esprit.tn>
2024-04-08 12:39:32 +0100

    service+models

---
+++ src/app/Models/project.ts
@@ -0,0 +1,14 @@
+import { StatusProject } from "./status-project";
+import { Task } from "./task";
+
+export class Project {
+    projectId!: number ;
+    projectName!:string;
+    projectDescription!:string;
+    startdateProject!:Date;
+    enddateProject!:Date;
+    projectStatus!:StatusProject;
+    tasks!:Task[];
+
+   
+}
---
+++ src/app/Services/task.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Task } from '../Models/task';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  urlTaskCrud:string="http://localhost:8082"
+
+  constructor(private myHttp:HttpClient) { }
+  getAllTasks():Observable<Task[]>{
+    return this.myHttp.get<Task[]>(this.urlTaskCrud + '/Task/GetAllTasks'); 
+  }
+
+  getPaginatedTasks(page: number = 0, pageSize: number = 10): Observable<Task[]> {
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('pageSize', pageSize.toString());
+    return this.myHttp.get<Task[]>(`${this.urlTaskCrud}/tasksbythree`, { params });
+  }
+
+  getTaskById(id:number):Observable<Task>{
+    return this.myHttp.get<Task>(this.urlTaskCrud +"/Task/GetTaskbyid?Taskid="+id);
+  }
+
+  AddTask(t:Task):Observable<Task>{
+    return this.myHttp.post<Task>(this.urlTaskCrud +'/Task/AddTask' ,t);
+  }
+
+  UpdateTask(t:Task):Observable<Task>{
+    return this.myHttp.put<Task>(this.urlTaskCrud +'/Task/UpdateTask' ,t);
+  }
+
+  deleteTask(id:number):Observable<void>{
+    return this.myHttp.delete<void>(this.urlTaskCrud +'/Task/DeleteTaskbyid?Taskid='+id);
+  }
+
+  getTasksByProjectId( projectId:number):Observable<Task[]>{
+    return this.myHttp.get<Task[]>(this.urlTaskCrud +"/Task/getTasksbyproject?projectId="+projectId);
+  }
+
+  addTaskWithProject(task: Task, projectName: string): Observable<void> {
+    return this.myHttp.put<void>(`${this.urlTaskCrud}/Task/affecterTaskaunprojet?taskname=`, { task, projectName });
+  }
+
+
+  getAllProjectNames():Observable<string[]>{
+    return this.myHttp.get<string[]>(this.urlTaskCrud + '/Task/getprojectNames'); 
+  }
+  getTasksByStatus(status: string): Observable<Task[]> {
+    return this.myHttp.get<Task[]>(`${this.urlTaskCrud}/Task/tasks?status=${status}`);
+  }
+
+  
+
+
+}
