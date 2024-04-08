19d89abb538ea63edd1aa75b5872a63ac777196a
thamer-souelmi <thamer.souelmi@esprit.tn>
2024-04-08 06:49:53 +0100

    Template Back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AllTemplateFrontComponent } from './FrontOffice/all-template-front/all-template-front.component';
-import { FooterFrontComponent } from './FrontOffice/footer-front/footer-front.component';
-import { HeaderFrontComponent } from './FrontOffice/header-front/header-front.component';
+import { FooterFrontComponent} from "./FrontOffice/footer-front/footer-front.component";
+import { HeaderFrontComponent} from "./FrontOffice/header-front/header-front.component";
 import { HomeFrontComponent } from './FrontOffice/home-front/home-front.component';
+import { SidebarBackComponent } from './BackOffice/sidebar-back/sidebar-back.component';
+import { NavbarBackComponent } from './BackOffice/navbar-back/navbar-back.component';
+import { FooterBackComponent } from './BackOffice/footer-back/footer-back.component';
+import { AllTemplatBackComponent } from './BackOffice/all-templat-back/all-templat-back.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatInputModule} from "@angular/material/input";
+import {MatRippleModule} from "@angular/material/core";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+
+import { FeatherModule } from 'angular-feather';
+import { allIcons } from 'angular-feather/icons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,11 +28,23 @@ import { HomeFrontComponent } from './FrontOffice/home-front/home-front.componen
     AllTemplateFrontComponent,
     FooterFrontComponent,
     HeaderFrontComponent,
-    HomeFrontComponent
+    HomeFrontComponent,
+    SidebarBackComponent,
+    NavbarBackComponent,
+    FooterBackComponent,
+    AllTemplatBackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatRippleModule,
+    MatMenuModule,
+    MatButtonModule,
+    FeatherModule.pick(allIcons),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
